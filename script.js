476feec5715ce0d9d6c58df22745f246bc060497
htweko99 <htweko2599@gmail.com>
2022-08-13 13:07:36 +0630

    Responsive Design

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 const chart = document.querySelector("#chart");
 
 var list = "";
+var days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
 
-fetch("./data.json").then(responese => responese.json()).then(
-    data => {
-        for(let i = 0; i< data.length; i++) {
-            list += `
-            <div class="data d-flex">
-              <div class="bar" style="height: ${data[i]['amount'] * 3}px;"></div>
-              <div class="amount">$${data[i]['amount']}</div>
-              <span class="day">${data[i]['day']}</span>
-            </div>
-            `
-        }
-        chart.innerHTML = list;
+fetch("./data.json")
+  .then((responese) => responese.json())
+  .then((data) => {
+    const d = new Date();
+
+    for (let i = 0; i < data.length; i++) {
+      if (days[d.getDay()] == data[i]["day"]) {
+        list += `
+                <div class="data d-flex">
+                    <div class="amount">$${data[i]["amount"]}</div>
+                    <div class="bar current-day" style="height: ${
+                      data[i]["amount"] * 3
+                    }px;"></div>
+                    
+                    <span class="day">${data[i]["day"]}</span>
+                </div>
+            `;
+      } else {
+        list += `
+                <div class="data d-flex">
+                    <div class="amount">$${data[i]["amount"]}</div>
+                    <div class="bar" style="height: ${
+                      data[i]["amount"] * 3
+                    }px;"></div>
+                    
+                    <span class="day">${data[i]["day"]}</span>
+                </div>`;
+      }
     }
-)
+
+    chart.innerHTML = list;
+  });
